15c4514f2f20840067ad34a22501b6bc85ce5167
TJMFL <161985881+TJMFL@users.noreply.github.com>
2025-04-01 18:22:48 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -30,7 +30,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "client", "dist"),
+    outDir: path.resolve(__dirname, "../server/public"),
     emptyOutDir: true,
   },
 });
